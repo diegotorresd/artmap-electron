eba09e301fda351e7fc7f9c44f33ef1e2e3824ca
diegotorresd <diegotorresd@gmail.com>
2016-08-15 12:23:12 +0200

    Configurablemapview

--- README.md
+++ README.md
@@ -6,3 +6,18 @@ WINDOWS:
 ~~~
 .\node_modules\.bin\electron .
 ~~~
+
+OS X:
+start server:
+~~~
+lighttpd -f /usr/local/etc/lighttpd/lighttpd.conf
+~~~
+start electron:
+~~~
+./node_modules/.bin/electron .
+~~~
+
+## Misc commands
+Identify images
+ImageMagick: Identify
+Vips: vipsheader
--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const electron = require('electron')
 const {app, BrowserWindow} = electron
+const fs = require('fs')
 
 let win
 
 app.on('ready', () => {
   const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize
+  let maps = readConfig('./maps.json')
+  let m = maps["whistler"]
   win = new BrowserWindow({width, height})
-  win.loadURL(`file://${__dirname}/maptest_fullscreen.html`)
+  win.loadURL(`file://${__dirname}/maptest_fullscreen.html?m=${encodeURI(JSON.stringify(m))}`)
   win.on('closed', () => win = null)
 })
+
+function readConfig(configPath) {
+  let result = fs.readFileSync(configPath, 'utf8')
+  return JSON.parse(result);
+}
 
 app.on('window-all-closed', () => app.quit())
